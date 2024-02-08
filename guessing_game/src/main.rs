b44a2b45c30c8c6f7a74bebedbe5079ea835b261
RiWieland <rich.wieland73@gmail.com>
2024-02-08 19:42:28 +0100

    test

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("Guess a number!");
     println!("Please input your guess.");
     
+    
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
