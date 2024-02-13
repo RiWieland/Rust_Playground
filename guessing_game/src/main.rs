c59d7c2ad47170f592fbad8c21366e078ef27f91
RiWieland <rich.wieland73@gmail.com>
2024-02-13 19:33:50 +0100

    added ordering

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,31 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess a number!");
     println!("Please input your guess.");
-    
+
+    let mut guess = String::new();
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
-    
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    let guess : u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("too Small"),
+        Ordering::Equal => println!("Equal"),
+        Ordering::Greater => println!("too great"),
+
+    }
 
 }
